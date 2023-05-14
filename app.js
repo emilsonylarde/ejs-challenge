41b504d693c21fd7bee09077f93fca98a5d74c76
EmilsonJJ <emilsonbreaker28@gmail.com>
2023-05-14 23:28:14 +0800

    Finished the project added compose function

--- app.js
+++ app.js
@@ -53,9 +53,10 @@ app.get("/posts/:postName", function(req,res) {
     const storedTitle = _.lowerCase(post.title);
 
     if(storedTitle === requestedTitle){
-      console.log("Match Found!");
-    }else {
-      console.log("Not a match!");
+     res.render("post", {
+      title:post.title,
+      body:post.body
+     })
     }
   });
 });
